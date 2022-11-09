021cdfce83a5d7d76be8ab0ebfef675622f09c4e
Ryan Hatamosa <pinoydol.colada@gmail.com>
2022-11-09 14:19:10 -0500

    added javascript fundamental notes

---
+++ fundamentals.js
@@ -0,0 +1,27 @@
+// var y;
+// console.log("Y: " + y);
+
+// function example() {
+//   var x;
+
+//   console.log("X: " + x);
+
+//   x = "Hello";
+//   console.log("X: " + x);
+// }
+
+// y = 10;
+
+// console.log("Y: " + y);
+
+// example();
+
+for (let i = 0; i < 10; i++) {
+  console.log("Running in loop: " + i);
+}
+
+console.log("Running outside the loop: " + i);
+
+i = 500;
+
+console.log("Running outside the loop: " + i);
